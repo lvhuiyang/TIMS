1fab176fd5555265254e70a53f1e9830c511304e
lvhuiyang <i@lvhuiyang.cn>
2017-06-21 08:30:56 +0800

    add some pages& add checkmixin

--- TIMS/urls.py
+++ TIMS/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
-from application.views import Index, Hello, Login
+from application.views import Base, Index, Login
 
+app_name = 'tims'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', Index.as_view(), name="index"),
-    url(r'^hello/$', Hello.as_view(), name="hello"),
-    url(r'^login/$', Login.as_view(), name="hello"),
+    url(r'^base/$', Base.as_view(), name="base"),
+    url(r'^login/$', Login.as_view(), name="login"),
+
 ]
--- application/views.py
+++ application/views.py
@@ -1,18 +1,28 @@
 from django.views import View
-from django.views.generic import TemplateView
 from django.shortcuts import render
 
 
-class Index(View):
+class CheckUserAuthenticatedMixin(object):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return render(request=request, template_name="login.html", context={"check_authenticated": True})
+        else:
+            return super(CheckUserAuthenticatedMixin, self).dispatch(request, *args, **kwargs)
+
+
+class Base(CheckUserAuthenticatedMixin, View):
+    def get(self, request, *args, **kwargs):
+        return render(request=request, template_name="base.html")
+
+
+class Index(CheckUserAuthenticatedMixin, View):
     def get(self, request, *args, **kwargs):
         return render(request=request, template_name="index.html", context={'hello': 'world'})
 
 
-class Hello(TemplateView):
+class Login(View):
     def get(self, request, *args, **kwargs):
-        return render(request=request, template_name="base.html", context={'hello': 'world'})
+        return render(request=request, template_name="login.html", context={"check_authenticated": False})
 
-
-class Login(TemplateView):
-    def get(self, request, *args, **kwargs):
-        return render(request=request, template_name="login.html", context={'hello': 'world'})
+    def post(self, request, *args, **kwargs):
+        return render(request=request, template_name="my_info.html", context={})
